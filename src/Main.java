bfb2019ce6cd2261b34514cfd79b35bcb782d643
HomeMedia <taknado13@yandex.ru>
2024-03-14 09:43:55 +0700

    решение ( исправленное )

--- src/Main.java
+++ src/Main.java
@@ -37,11 +37,15 @@ public class Main {
         System.out.println("Средняя сумма трат за месяц составила " + expensesAverage + " рублей");
         // Задание 4
         System.out.println("Задание 4");
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--){
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            char temp = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - i - 1];
+            reverseFullName[reverseFullName.length - i - 1] = temp;
+        }
+        for (int i = 0; i < reverseFullName.length; i++) {
             System.out.print(reverseFullName[i]);
         }
-
 
 
     }
